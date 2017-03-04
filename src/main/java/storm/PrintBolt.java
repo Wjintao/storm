feb41305a3288c4b5d8a3f73f1726424aa7d7591
wujintao <1443304718@qq.com>
2017-03-04 12:41:24 +0800

    strom运用

---
+++ src/main/java/storm/PrintBolt.java
@@ -0,0 +1,32 @@
+package storm;
+
+import java.util.Map;
+
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Tuple;
+
+
+public class PrintBolt extends BaseBasicBolt {
+
+    private int indexId;
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context) {
+        this.indexId = context.getThisTaskIndex();
+    }
+
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        String rec = tuple.getString(0);
+        System.err.println(String.format("Bolt[%d] String recieved: %s", this.indexId, rec));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        // do nothing
+    }
+
+}
